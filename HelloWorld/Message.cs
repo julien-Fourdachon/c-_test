58a7086227d0220b3ecee3d20ac27e6288efcb1e
artatzar <ahmed_slimani@live.fr>
2018-06-19 16:30:56 +0200

    test c#

---
+++ HelloWorld/Message.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HelloWorld
+{
+    class Message
+    {
+
+        public int date =  (int) DateTime.Now.DayOfWeek;
+        public int heure = (int)DateTime.Now.Hour;
+        
+        public string message;
+
+        public int Morning { get; set; }
+        public int Afternoon { get; set; }
+        private int Night { get; set; }
+        
+
+        public Message (int mornin = 8, int aftern = 13, int evening = 18)
+        {
+            SetMorning(mornin);
+            SetAfter(aftern);
+            SetNight(evening);
+        }
+
+        public void SetMorning(int mornin)
+        {
+            Morning = mornin;
+        }
+
+        public void SetNight(int evening)
+        {
+            Night = evening;
+        }
+
+        public void SetAfter(int aftern)
+        {
+            Afternoon = aftern;
+        }
+
+        public void GetMorning()
+        {
+
+        }
+
+        public string GetHelloMessage()
+        {
+            if(date == 6 ||  (date == 5 && heure >=this.Night) || (date == 1 && heure <this.Morning))
+            {
+                message = "Bon Week End ";
+            }
+            else if (heure >=9 && heure < this.Afternoon)
+            {
+                message = "Bonjour ";
+            }
+            else if (heure >=this.Afternoon && heure < this.Night)
+            {
+                message = "Bon après midi ";
+            }
+            else
+            {
+                message = "Bonsoir ";
+            }
+
+            return message;
+ 
+        }
+        
+       
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Veuillez saisir l'heure du bonjour");
+            String result = ReadLine();
+            Int32.TryParse(result, out int choice);
+
+            
+            WriteLine("Veuillez saisir l'heure du bon après");
+            String result1 = ReadLine();
+            Int32.TryParse(result1, out int choice1);
+
+            WriteLine("Veuillez saisir l'heure du bonsoir");
+            String result2 = ReadLine();
+            Int32.TryParse(result, out int choice2);
+
+
+
+            Message msg = new Message(choice, choice1, choice2);
+            String message = msg.GetHelloMessage();
+
+            String response = "";
+
+            do
+            {
+                WriteLine(msg.message + " " + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+                WriteLine("Veuillez saisir \"exit\" pour sortir");
+                response = ReadLine();
+                            }
+
+            while (!response.Equals("exit"));
+                      
+                
+
+        }
+
+
+    }
+}
